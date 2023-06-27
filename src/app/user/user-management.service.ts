082bde9f0edb709a5774818ea05757818d518d35
karakuscem <karakuscemenes@gmail.com>
2023-06-27 23:18:13 +0300

    code improvement

--- src/app/user/user-management.service.ts
+++ src/app/user/user-management.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
+import { usersInfo } from 'src/assets/users';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class UserManagementService {
 
   getUsers(): User[] {
     if (localStorage.getItem('users') !== null) {
-      this.users = JSON.parse(localStorage.getItem('users')!);
+      return this.users;
     } else {
       this.setUsers();
     }
@@ -17,78 +18,7 @@ export class UserManagementService {
   }
 
   setUsers(): void {
-    this.users =         [
-      {
-        ID: 1,
-        USERNAME: "ifintoph0",
-        EMAIL: "asantello0@wordpress.com",
-        DATE: new Date("10/7/2020").toLocaleDateString(),
-        ACTIVE: true
-      },
-      {
-        ID: 2,
-        USERNAME: "hstanyan1",
-        EMAIL: "dkaas1@flickr.com",
-        DATE: new Date("6/6/2023").toLocaleDateString(),
-        ACTIVE: true
-      },
-      {
-        ID: 3,
-        USERNAME: "gpossa2",
-        EMAIL: "ejarrette2@kickstarter.com",
-        DATE: new Date("11/14/2020").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 4,
-        USERNAME: "ycanton3",
-        EMAIL: "lsimenot3@bizjournals.com",
-        DATE: new Date("11/1/2020").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 5,
-        USERNAME: "rmcterry4",
-        EMAIL: "chemphall4@sciencedirect.com",
-        DATE: new Date("8/28/2021").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 6,
-        USERNAME: "gkorf5",
-        EMAIL: "slegat5@huffingtonpost.com",
-        DATE: new Date("6/29/2022").toLocaleDateString(),
-        ACTIVE: true
-      },
-      {
-        ID: 7,
-        USERNAME: "swhittier6",
-        EMAIL: "nring6@csmonitor.com",
-        DATE: new Date("9/16/2020").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 8,
-        USERNAME: "adownton7",
-        EMAIL: "pvarfolomeev7@youtube.com",
-        DATE: new Date("2/19/2021").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 9,
-        USERNAME: "abosenworth8",
-        EMAIL: "epaprotny8@go.com",
-        DATE: new Date("9/6/2021").toLocaleDateString(),
-        ACTIVE: false
-      },
-      {
-        ID: 10,
-        USERNAME: "cshory9",
-        EMAIL: "mregitz9@patch.com",
-        DATE: new Date("7/24/2022").toLocaleDateString(),
-        ACTIVE: false
-      }
-    ];
+    this.users = usersInfo;
     localStorage.setItem('users', JSON.stringify(this.users));
   }
 
