600c4b4c279f38bc3d319757414d43ad2f54da4d
karakuscem <karakuscemenes@gmail.com>
2023-06-28 00:01:35 +0300

    pagination added, need fixes

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -8,8 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class TableComponent {
   @Input() columns: string[] = [];
   @Input() data: any[] = [];
-  @Output() onDeleteClick =  new EventEmitter<any>();
-  @Output() onEditClick =  new EventEmitter<any>();
+  @Output() onDeleteClick = new EventEmitter<any>();
+  @Output() onEditClick = new EventEmitter<any>();
+
+  pagedData: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 10;
+
+  ngOnChanges(): void {
+    this.pageChanged(1);
+  }
 
   getObjectValues(obj: any): any[] {
     return Object.entries(obj).map(([key, value]) => `${value}`);
@@ -23,4 +31,27 @@ export class TableComponent {
     this.onEditClick.emit(id);
   }
 
+  pageChanged(page: number): void {
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.pagedData = this.data.slice(startIndex, endIndex);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.pageChanged(this.currentPage);
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.pageChanged(this.currentPage);
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.data.length / this.itemsPerPage);
+  }
 }
